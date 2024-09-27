a5dd014e923ac51d918e31345dfd8bf0e83fec79
Mallikarjun <rjun912@gmail.com>
2024-09-27 10:32:04 +0530

    second commit made some changes

--- testgitproject/src/sample/TestRepo.java
+++ testgitproject/src/sample/TestRepo.java
@@ -3,5 +3,8 @@ package sample;
 public class TestRepo {
 	public static void main(String[] args) {
 		System.out.println("This is a sample GitRepo example"); 
+		System.out.println("add = "+(10+20));
+		System.out.println("this is another line");   
+		
 	}
 }
